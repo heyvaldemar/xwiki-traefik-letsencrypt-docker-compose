6e265a3ab65e36f8b65b010da684d6608fb557ac
Vladimir Mikhalev <ask@sre.gg>
2023-01-20 12:35:46 -0500

    XWiki with Let's Encrypt in a Docker Compose

--- xwiki-restore-database.sh
+++ xwiki-restore-database.sh
@@ -23,7 +23,7 @@ docker stop $XWIKI_CONTAINER
 
 echo "--> Restoring database..."
 docker exec -it $XWIKI_BACKUPS_CONTAINER sh -c 'PGPASSWORD="$(echo $POSTGRES_PASSWORD)" dropdb -h postgres -p 5432 xwiki -U xwikidbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" createdb -h postgres -p 5432 xwikidb -U xwikidbuser \
+&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" createdb -h postgres -p 5432 xwiki -U xwikidbuser \
 && PGPASSWORD="$(echo $POSTGRES_PASSWORD)" gunzip -c /srv/xwiki-postgres/backups/'$SELECTED_DATABASE_BACKUP' | PGPASSWORD=$(echo $POSTGRES_PASSWORD) psql -h postgres -p 5432 xwikidb -U xwikidbuser'
 echo "--> Database recovery completed..."
 
