e7e6e0d2b7ed7dc7654cb46609d6044442a0ae13
Vladimir Mikhalev <ask@sre.gg>
2023-09-18 23:30:18 -0400

    XWiki with Let's Encrypt Using Docker Compose

--- xwiki-restore-database.sh
+++ xwiki-restore-database.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-XWIKI_CONTAINER=$(docker ps -aqf "name=xwiki")
-XWIKI_BACKUPS_CONTAINER=$(docker ps -aqf "name=xwiki_backups")
+# # xwiki-restore-database.sh Description
+# This script facilitates the restoration of a database backup.
+# 1. **Identify Containers**: It first identifies the service and backups containers by name, finding the appropriate container IDs.
+# 2. **List Backups**: Displays all available database backups located at the specified backup path.
+# 3. **Select Backup**: Prompts the user to copy and paste the desired backup name from the list to restore the database.
+# 4. **Stop Service**: Temporarily stops the service to ensure data consistency during restoration.
+# 5. **Restore Database**: Executes a sequence of commands to drop the current database, create a new one, and restore it from the selected compressed backup file.
+# 6. **Start Service**: Restarts the service after the restoration is completed.
+# To make the `xwiki-restore-database.shh` script executable, run the following command:
+# `chmod +x xwiki-restore-database.sh`
+# Usage of this script ensures a controlled and guided process to restore the database from an existing backup.
+
+XWIKI_CONTAINER=$(docker ps -aqf "name=xwiki-xwiki")
+XWIKI_BACKUPS_CONTAINER=$(docker ps -aqf "name=xwiki-backups")
+XWIKI_DB_NAME="xwiki"
+XWIKI_DB_USER="xwikidbuser"
+POSTGRES_PASSWORD=$(docker exec $XWIKI_BACKUPS_CONTAINER printenv PGPASSWORD)
+BACKUP_PATH="/srv/xwiki-postgres/backups/"
 
 echo "--> All available database backups:"
 
-for entry in $(docker container exec -it $XWIKI_BACKUPS_CONTAINER sh -c "ls /srv/xwiki-postgres/backups/")
+for entry in $(docker container exec "$XWIKI_BACKUPS_CONTAINER" sh -c "ls $BACKUP_PATH")
 do
   echo "$entry"
 done
@@ -19,13 +35,13 @@ read SELECTED_DATABASE_BACKUP
 echo "--> $SELECTED_DATABASE_BACKUP was selected"
 
 echo "--> Stopping service..."
-docker stop $XWIKI_CONTAINER
+docker stop "$XWIKI_CONTAINER"
 
 echo "--> Restoring database..."
-docker exec -it $XWIKI_BACKUPS_CONTAINER sh -c 'PGPASSWORD="$(echo $POSTGRES_PASSWORD)" dropdb -h postgres -p 5432 xwiki -U xwikidbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" createdb -h postgres -p 5432 xwiki -U xwikidbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" gunzip -c /srv/xwiki-postgres/backups/'$SELECTED_DATABASE_BACKUP' | PGPASSWORD=$(echo $POSTGRES_PASSWORD) psql -h postgres -p 5432 xwikidb -U xwikidbuser'
+docker exec "$XWIKI_BACKUPS_CONTAINER" sh -c "dropdb -h postgres -p 5432 $XWIKI_DB_NAME -U $XWIKI_DB_USER \
+&& createdb -h postgres -p 5432 $XWIKI_DB_NAME -U $XWIKI_DB_USER \
+&& gunzip -c ${BACKUP_PATH}${SELECTED_DATABASE_BACKUP} | psql -h postgres -p 5432 $XWIKI_DB_NAME -U $XWIKI_DB_USER"
 echo "--> Database recovery completed..."
 
 echo "--> Starting service..."
-docker start $XWIKI_CONTAINER
+docker start "$XWIKI_CONTAINER"
